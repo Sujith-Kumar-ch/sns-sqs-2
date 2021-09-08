8eb72c7b48838688e102eff34e7f8532406674fa
Sujith-Kumar-ch <86654454+Sujith-Kumar-ch@users.noreply.github.com>
2021-09-08 18:35:59 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -3,17 +3,17 @@ terraform {
   # Terraform version at the time of writing this post
   required_version = ">= 0.12.24"
 
+ terraform {
   backend "s3" {
     bucket = "sns-sqs-1"
-    key    = "quickcloudpocsbackend_2.tfstate"
+    key    = "path/terraform.tfstate"
     region = "ap-south-1"
+
   }
 }
-
 ## random provider
 provider "random" {}
 
-## Provider us-east-1
 provider "aws" {
   region = "ap-south-1"
 }
